17342673e7e20c13a3ad5f8eb5fb9afe35434c26
Isaia-mhd <mohamedesaie21@gmail.com>
2025-06-04 21:08:04 +0300

    make some changes

--- app/Http/Controllers/BilanController.php
+++ app/Http/Controllers/BilanController.php
@@ -9,20 +9,17 @@ class BilanController extends Controller
 {
     public function index()
     {
-        $amounts = Pret::pluck("totalPayer");
-
         $minPayer = Pret::min('totalPayer');
         $maxPayer = Pret::max('totalPayer');
         $total = Pret::sum('totalPayer');
 
 
-
-        return response()->json([
-            "minAPayer" => $minPayer,
-            "maxAPayer" => $maxPayer,
-            "totalAPayer" => $total,
-            "aomunt" => $amounts,
-
-        ], 200);
+        return response()->json(
+            ["bilans" => [
+                "minAPayer" => $minPayer,
+                "maxAPayer" => $maxPayer,
+                "totalAPayer" => $total,
+                ]
+            ], 200);
     }
 }
